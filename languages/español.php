d95a7057f4ea8273b6d53987b7a409442c54ac1c
codefir <pricop2008@yahoo.com>
2019-02-04 04:55:01 +0200

    Add files via upload

---
+++ languages/español.php
@@ -0,0 +1,282 @@
+<?php
+// Language Name
+$name = 'Español';
+
+// Language Author
+$author = 'phpSearch';
+
+// Language URL
+$url = 'http://phpsearch.com';
+
+// Language Direction
+$lang['lang_dir'] = 'ltr';
+
+// Menu
+$lang['search'] = 'Buscar';
+$lang['web'] = 'Web';
+$lang['images'] = 'Imágenes';
+$lang['videos'] = 'Videos';
+$lang['news'] = 'Noticias';
+
+$lang['info'] = 'Info';
+$lang['themes'] = 'Temas';
+$lang['language'] = 'Idioma';
+$lang['languages'] = 'Idiomas';
+
+// Admin Panel
+$lang['admin'] = 'Admin';
+$lang['general'] = 'General';
+$lang['appearance'] = 'Apariencia';
+$lang['info_pages'] = 'Páginas de Info';
+$lang['username'] = 'Nombre de usuario';
+$lang['password'] = 'Contraseña';
+$lang['login'] = 'Iniciar sesión';
+$lang['logout'] = 'Cerrar sesión';
+$lang['save'] = 'Salvar';
+$lang['remember_me'] = 'Recuérdame';
+$lang['recover'] = 'Recuperar';
+$lang['by'] = 'De';
+$lang['active'] = 'Activo';
+$lang['activate'] = 'Activar';
+$lang['default'] = 'Predeterminado';
+$lang['make_default'] = 'Hacer Predeterminado';
+$lang['public'] = 'Público';
+$lang['unlisted'] = 'No listado';
+$lang['edit'] = 'Editar';
+$lang['new_page'] = 'Nueva pagina';
+$lang['delete'] = 'Borrar';
+$lang['cancel'] = 'Cancelar';
+$lang['yes'] = 'Sí';
+$lang['no'] = 'No';
+$lang['off'] = 'Apagado';
+$lang['on'] = 'Encendido';
+
+$lang['dashboard'] = 'Tablero';
+$lang['site_title'] = 'Título del sitio';
+$lang['site_tagline'] = 'Lema';
+$lang['logo_small'] = 'Logo pequeño';
+$lang['logo_large'] = 'Logo grandee';
+$lang['favicon'] = 'Favicon';
+$lang['backgrounds'] = 'Fondos';
+$lang['timezone'] = 'Zona horaria';
+$lang['tracking_code'] = 'Codigo de localización';
+$lang['current_password'] = 'Contraseña actual';
+$lang['new_password'] = 'Nueva contraseña';
+$lang['confirm_new_password'] = 'Confirmar nueva contraseña';
+
+$lang['settings_saved'] = 'Se guardó la configuración';
+
+$lang['upload_error_code'] = 'Código de error de carga: %s';
+$lang['invalid_user_pass'] = 'Usuario o contraseña invalido';
+$lang['wrong_current_password'] = 'La contraseña actual que has introducido no es válida';
+$lang['password_too_short'] = 'La contraseña es demasiado corta';
+$lang['password_not_matching'] = 'La contraseña no coincide';
+$lang['all_fields_required'] = 'Todos los campos son obligatorios';
+
+$lang['page_title'] = 'Título de la Página';
+$lang['page_url'] = 'URL de la Página';
+$lang['page_public'] = 'Página Pública';
+$lang['page_content'] = 'Contenido de Página';
+$lang['page_url_exists'] = 'La URL de la página ya existe';
+$lang['page_created'] = 'Se ha creado la página %s';
+$lang['page_deleted'] = 'La página %s ha sido eliminada';
+
+$lang['site_info'] = 'Información del sitio';
+$lang['useful_links'] = 'Enlaces útiles';
+$lang['get_more_themes'] = 'Obtener mas temas';
+$lang['get_more_languages'] = 'Obtener mas idiomas';
+
+$lang['search_api_key'] = 'Clave API';
+$lang['web_per_page'] = 'Resultados web / página';
+$lang['images_per_page'] = 'Imágenes de resultados / página';
+$lang['videos_per_page'] = 'Videos de resultados / página';
+$lang['news_per_page'] = 'Resultados de noticias / página';
+$lang['searches_per_ip'] = 'Búsquedas / IP';
+$lang['search_suggestions'] = 'Sugerencias de búsqueda';
+$lang['search_entities'] = 'Buscar Entidades';
+$lang['search_answers'] = 'Buscar Respuestas';
+$lang['suggestions_per_ip'] = 'Sugerencias / IP';
+$lang['search_specific_sites'] = 'Buscar solo en sitios específicos';
+$lang['one_domain_line'] = 'Un dominio por línea';
+
+$lang['ads'] = 'Anuncios';
+$lang['ads_safe'] = 'Anuncios seguros';
+$lang['ads_1'] = 'Página de inicio';
+$lang['ads_2'] = 'Página de búsqueda (Superior)';
+$lang['ads_3'] = 'Search page (Inferior)';
+$lang['ad_unit_code'] = 'Código de bloque de anuncios';
+
+// Home
+$lang['preferences'] = 'Preferencias';
+
+// Preferences
+$lang['theme'] = 'Tema';
+$lang['region'] = 'Región';
+$lang['dark_mode'] = 'Modo oscuro';
+$lang['center_content'] = 'Centrar Contenido';
+$lang['site_language'] = 'Idioma del sitio';
+$lang['new_window'] = 'Abrir resultados en una nueva ventana/pestaña';
+$lang['highlight'] = 'Resaltar palabras clave en los resultados de búsqueda';
+
+// Misc
+$lang['token_mismatch'] = 'Ficha invalida';
+$lang['copyright'] = '&copy; %s %s';
+$lang['powered_by'] = 'Powered by %s';
+
+// Search
+$lang['filters'] = 'Filtros';
+$lang['period'] = 'Período';
+$lang['past_day'] = 'Dia pasado';
+$lang['past_week'] = 'Semana pasada';
+$lang['past_month'] = 'Mes pasado';
+$lang['past_year'] = 'Año pasado';
+
+$lang['type'] = 'Tipo';
+$lang['duration'] = 'Duración';
+$lang['quality'] = 'Calidad';
+$lang['all'] = 'Todos';
+
+$lang['safe_search'] = 'Búsqueda Segura';
+$lang['moderate'] = 'Moderado';
+$lang['strict'] = 'Estricto';
+$lang['short'] = 'Corto';
+$lang['medium'] = 'Medio';
+$lang['long'] = 'Largo';
+
+$lang['aspect'] = 'Aspecto';
+$lang['animated'] = 'Animado';
+$lang['clipart'] = 'Prediseñadas';
+$lang['line'] = 'Dibujo';
+$lang['photo'] = 'Foto';
+$lang['transparent'] = 'Transparente';
+$lang['square'] = 'Cuadrado';
+$lang['wide'] = 'Amplio';
+$lang['tall'] = 'Alto';
+$lang['size'] = 'Tamaño';
+$lang['small'] = 'Pequeño';
+$lang['large'] = 'Grande';
+$lang['wallpaper'] = 'Fondo';
+$lang['color'] = 'Color';
+$lang['colored'] = 'Colorado';
+$lang['monochrome'] = 'Monocromo';
+$lang['license'] = 'Licencia';
+$lang['share'] = 'Compartir';
+$lang['share_commercially'] = 'Compartir Comercialmente';
+$lang['modify'] = 'Modificar';
+$lang['modify_commercially'] = 'Modificar Comercialmente';
+
+$lang['sort_by'] = 'Ordenar por';
+$lang['relevance'] = 'Pertinencia';
+$lang['date'] = 'Fecha';
+
+$lang['source'] = 'Origen';
+$lang['image'] = 'Imagen';
+
+$lang['search_l_e'] = 'Has superado el límite de búsqueda diaria, inténtalo de nuevo mañana.';
+$lang['no_results_found'] = 'No se encontraron resultados para %s.';
+$lang['suggestions'] = 'Sugerencias:';
+$lang['suggestion_1'] = 'Pruebe palabras clave menos específicas.';
+$lang['suggestion_2'] = 'Pruebe diferentes palabras clave.';
+$lang['suggestion_3'] = 'Pruebe menos palabras clave.';
+
+$lang['showing_results_for'] = 'Mostrando resultados para %s';
+$lang['search_instead_for'] = 'Buscar en lugar de %s';
+$lang['related_searches'] = 'Búsquedas Relacionadas';
+$lang['more_x'] = 'Mas %s';
+$lang['x_results'] = '%s resultados';
+
+$lang['data_from'] = 'Datos de:';
+$lang['text_under'] = 'Texto bajo %s';
+$lang['read_more'] = 'Lee mas';
+$lang['see_results_for'] = 'Ver resultados para';
+
+$lang['date_format'] = '%3$s %2$s, %1$s';
+
+$lang['views_x'] = '%s visualizaciones';
+$lang['views_k'] = 'K';
+$lang['views_m'] = 'M';
+$lang['views_b'] = 'B';
+$lang['views_t'] = 'T';
+$lang['month_01'] = 'Enero';
+$lang['month_02'] = 'Febrero';
+$lang['month_03'] = 'Marzo';
+$lang['month_04'] = 'Abril';
+$lang['month_05'] = 'Mayo';
+$lang['month_06'] = 'Junio';
+$lang['month_07'] = 'Julio';
+$lang['month_08'] = 'Agosto';
+$lang['month_09'] = 'Septiembre';
+$lang['month_10'] = 'Octubre';
+$lang['month_11'] = 'Noviembre';
+$lang['month_12'] = 'Diciembre';
+
+$lang['decimals_separator'] = ',';
+$lang['thousands_separator'] = '.';
+$lang['ellipsis'] = '...';
+
+// Instant Answers triggers
+$lang['ia']['ip'][] = 'cuál es mi ip';
+$lang['ia']['ip'][] = 'mi ip';
+$lang['ia']['ip'][] = 'ip';
+$lang['ia']['time'][] = 'hora actual';
+$lang['ia']['time'][] = 'hora local';
+$lang['ia']['time'][] = 'hora';
+$lang['ia']['date'][] = 'fecha actual';
+$lang['ia']['date'][] = 'fecha local';
+$lang['ia']['date'][] = 'fecha';
+$lang['ia']['flip_coin'][] = 'moneda de tirón';
+$lang['ia']['flip_coin'][] = 'cara o cruz';
+$lang['ia']['stopwatch'][] = 'cronógrafo';
+$lang['ia']['stopwatch'][] = 'cronómetro';
+$lang['ia']['roll'][] = 'roll';
+$lang['ia']['qr_code'][] = 'qr';
+$lang['ia']['qr_code'][] = 'código qr';
+$lang['ia']['sort_asc'][] = 'ordenar';
+$lang['ia']['sort_asc'][] = 'orden ascendente';
+$lang['ia']['sort_desc'][] = 'orden descendiente';
+$lang['ia']['reverse_text'][] = 'texto inverso';
+$lang['ia']['md5'][] = 'md5';
+$lang['ia']['base64_encode'][] = 'codificar base64';
+$lang['ia']['base64_decode'][] = 'decodificar base64';
+$lang['ia']['lowercase'][] = 'minúsculas';
+$lang['ia']['uppercase'][] = 'mayúsculas';
+$lang['ia']['leap_year'][] = 'año bisiesto';
+$lang['ia']['screen_resolution'][] = 'resolución de la pantalla';
+$lang['ia']['pi'][] = 'pi';
+$lang['ia']['pi'][] = 'π';
+$lang['ia']['morse_code'][] = 'código morse';
+$lang['ia']['unix_time'][] = 'tiempo de unix';
+$lang['ia']['lorem_ipsum'][] = 'lorem ipsum';
+$lang['ia']['atbash'][] = 'atbash';
+
+// Cards
+$lang['your_ip_is'] = 'Tu IP es';
+$lang['current_time_is'] = 'La hora actual es';
+$lang['current_date_is'] = 'La fecha actual es';
+$lang['you_have_flipped'] = 'Has tirado';
+$lang['coin_0'] = 'Cara';
+$lang['coin_1'] = 'Cruz';
+$lang['stopwatch'] = 'Cronómetro';
+$lang['start'] = 'Iniciar';
+$lang['stop'] = 'Parar';
+$lang['reset'] = 'Reiniciar';
+$lang['you_have_rolled'] = 'Has tirado';
+$lang['qr_code_for'] = 'Código QR para %s';
+$lang['sorted_1'] = 'Ordenado en orden ascendente';
+$lang['sorted_2'] = 'Ordenado en orden descendente';
+$lang['reversed_text'] = 'El texto invertido es';
+$lang['md5_hash_for'] = 'Hash MD5 para %s';
+$lang['base64_1'] = 'Codificación en Base64 para %s';
+$lang['base64_2'] = 'Decodificación en base64 para %s';
+$lang['case_1'] = 'Minúsculas';
+$lang['case_2'] = 'Mayúsculas';
+$lang['leap_year_1'] = 'Es un año bisiesto';
+$lang['leap_year_2'] = 'No es un año bisiesto';
+$lang['your_sr_is'] = 'Su resolución de pantalla es';
+$lang['pi_is'] = 'El valor de Pi es';
+$lang['morse_code_for'] = 'El código morse para %s';
+$lang['unix_time'] = 'Tiempo de Unix';
+$lang['unix_time_for'] = 'Date for %s Unix Time';
+$lang['gmt'] = 'GMT';
+$lang['x_lorem_ipsum'] = '%s párrafos del Lorem Ipsum';
+$lang['atbash_for'] = 'Cifrado Atbash para %s';
